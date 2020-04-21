fbbfd0995d501a588c237d07f96a6c55c6ce40d9
Criss Cheng <chrischengck10339@gmail.com>
2020-04-21 19:27:59 -0400

    finish comparing songs

--- app.py
+++ app.py
@@ -1,35 +1,29 @@
 # coding=utf8
-# track_dict = {'jp': {'top_1': {'track_id': 195214622, 'track_name': 'パラボラ', 'album_name': 'Parabola - Single', 'artist_name': 'Official HIGE DANdism', 'lyrics': 'ダンボールだらけから幕開けた日々は\n想像よりも少しだけ忙しく過ぎていってる\n\n片づくことを知らないこの部屋はなんだか\n他の誰かの暮らしから借りてきたみたいだ\n\nまっさらなノートの上\n一文字目を書き出すようにして\n期待感と不安感が混ざったインクに浸した心で\n\n互い違いに歩き出した僕の両足は\nどんな未来のアスファルト踏みしめていくんだろう\n靴底を擦り減らしてドアの向こう側\nまだ遠くて不確かでぼやけてる理想像も\n...\n\n******* This Lyrics is NOT for Commercial use *******\n(1409619538131)', 'lyrics_language': 'not available', 'yt_videoID': '6RHCbvzkXBc', 'yt_videoTitle': 'Official髭男dism - 新曲「パラボラ」フル\u3000［歌詞つき］', 'yt_url': 'https://www.youtube.com/watch?v=6RHCbvzkXBc', 'yt_view_counts': '741179', 'yt_like_counts': '10654', 'yt_dislike_counts': '228', 'yt_comment_counts': '9'}, 'top_2': {'track_id': 190577819, 'track_name': 'I LOVE...', 'album_name': 'I Love...', 'artist_name': 'Official HIGE DANdism', 'lyrics': '僕が見つめる 景色のその中に\n君が入ってから 変わり果てた世界は\nいつもそつなく こなした日々の真ん中\n不思議な引力に 逆らえず崩れてく\n\nI LOVE... なんて 言いかけてはやめて\nI LOVE... I LOVE... 何度も\n\n高まる愛の中 変わる心情の中\n燦然と輝く姿は\nまるで水槽の中に 飛び込んで\n溶けた絵の具みたいな\nイレギュラー\n\nひとりじゃ何ひとつ 気づけなかっただろう\nこんなに鮮やかな色彩に\n普通の事だと とぼける君に言いかけた\n...\n\n******* This Lyrics is NOT for Commercial use *******\n(1409619538131)', 'lyrics_language': 'not available', 'yt_videoID': 'bt8wNQJaKAk', 'yt_videoTitle': 'Official髭男dism - I LOVE...［Official Video］', 'yt_url': 'https://www.youtube.com/watch?v=bt8wNQJaKAk', 'yt_view_counts': '54003112', 'yt_like_counts': '419160', 'yt_dislike_counts': '9183', 'yt_comment_counts': '23653'}, 'top_3': {'track_id': 166787995, 'track_name': 'Harunohi', 'album_name': 'Harunohi', 'artist_name': 'Aimyon', 'lyrics': '北千住駅のプラットホーム\n銀色の改札\n思い出話と想い出ふかし\n腰掛けたベンチで\n\n僕らは何も見えない\n未来を誓い合った\n\n寒さにこらえた木々と猫が\nまるで僕らのことで\n蕾を咲かせようと身を揺らしてる\n素敵に笑っている\n\n焦らないでいい\nいつか花束になっておくれよ\n\nそれまで待っていてね\nこれからの展開を二人で\n飽きるまで過ごしてみるからね\n最低限の愛を伝えながら\n\nどんな未来が\nこちらを覗いてるかな\n...\n\n******* This Lyrics is NOT for Commercial use *******\n(1409619538131)', 'lyrics_language': 'not available', 'yt_videoID': 'pfGI91CFtRg', 'yt_videoTitle': 'あいみょん – ハルノヒ【OFFICIAL MUSIC VIDEO】', 'yt_url': 'https://www.youtube.com/watch?v=pfGI91CFtRg', 'yt_view_counts': '45265318', 'yt_like_counts': '166731', 'yt_dislike_counts': '7527', 'yt_comment_counts': '8426'}, 'top_4': {'track_id': 169172251, 'track_name': '紅蓮華', 'album_name': '紅蓮華', 'artist_name': 'LiSA', 'lyrics': "強くなれる理由を知った 僕を連れて進め\n\n泥だらけの走馬灯に酔う こわばる心\n震える手は掴みたいものがある それだけさ\n夜の匂いに (I'll spend all thirty nights)\n空睨んでも (Staring into the sky)\n変わっていけるのは自分自身だけ それだけさ\n\n強くなれる理由を知った 僕を連れて進め\n\nどうしたって！\n消せない夢も 止まれない今も\n誰かのために強くなれるなら\nありがとう 悲しみよ\n...\n\n******* This Lyrics is NOT for Commercial use *******\n(1409619538131)", 'lyrics_language': 'not available', 'yt_videoID': 'MpYy6wwqxoo', 'yt_videoTitle': 'LiSA - 紅蓮華 / THE FIRST TAKE', 'yt_url': 'https://www.youtube.com/watch?v=MpYy6wwqxoo', 'yt_view_counts': '33597780', 'yt_like_counts': '552774', 'yt_dislike_counts': '7203', 'yt_comment_counts': '23791'}, 'top_5': {'track_id': 190581171, 'track_name': '白日', 'album_name': 'CEREMONY', 'artist_name': 'King Gnu', 'lyrics': '時には誰かを\n知らず知らずのうちに\n傷つけてしまったり\n失ったりして初めて\n犯した罪を知る\n\n戻れないよ、昔のようには\n煌めいて見えたとしても\n明日へと歩き出さなきゃ\n雪が降りしきろうとも\n\n今の僕には\n何ができるの？\n何になれるの？\n誰かのために生きるなら\n正しいことばかり\n言ってらんないよな\n\nどこかの街で\nまた出逢えたら\n僕の名前を\n覚えていますか？\nその頃にはきっと\n春風が吹くだろう\n\n真っ新に生まれ変わって\n人生一から始めようが\nへばりついて離れない\n地続きの今を歩いているんだ\n\n真っ白に全てさよなら\n降りしきる雪よ\n全てを包み込んでくれ\n...\n\n******* This Lyrics is NOT for Commercial use *******\n(1409619538131)', 'lyrics_language': 'not available', 'yt_videoID': 'ony539T074w', 'yt_videoTitle': 'King Gnu - 白日', 'yt_url': 'https://www.youtube.com/watch?v=ony539T074w', 'yt_view_counts': '197266552', 'yt_like_counts': '1097195', 'yt_dislike_counts': '23716', 'yt_comment_counts': '79520'}}, 'tw': {'top_1': {'track_id': 192014585, 'track_name': '시작', 'album_name': 'ITAEWON CLASS (Original Television Soundtrack), Pt. 2 - Single', 'artist_name': 'Gaho', 'lyrics': '새로운 시작은 늘 설레게 하지\n모든 걸 이겨낼 것처럼\n시간을 뒤쫓는 시계바늘처럼\n앞질러 가고 싶어 하지\n\n그어 놓은 선을 넘어\n저마다 삶을 향해\n때론 원망도 하겠지\n그 선을 먼저 넘지 말라고\n\nI can fly the sky\nNever gonna stay\n내가 지쳐 쓰러질 때까진\n어떤 이유도\n어떤 변명도\n지금 내겐 용기가 필요해\n\n빛나지 않아도 내 꿈을 응원해\n...\n\n******* This Lyrics is NOT for Commercial use *******\n(1409619538131)', 'lyrics_language': 'not available', 'yt_videoID': 'IKOke_QKR0w', 'yt_videoTitle': 'Gaho (가호) - Start 시작 (Itaewon Class OST Part 2) Lyrics (Han/Rom/Eng/가사)', 'yt_url': 'https://www.youtube.com/watch?v=IKOke_QKR0w', 'yt_view_counts': '1838607', 'yt_like_counts': '35029', 'yt_dislike_counts': '242', 'yt_comment_counts': '704'}, 'top_2': {'track_id': 114728278, 'track_name': 'Astronomia', 'album_name': 'Astronomia', 'artist_name': 'Vicetone feat. Tony Igy', 'lyrics': '', 'lyrics_language': 'not available', 'yt_videoID': 'iLBBRuVDOo4', 'yt_videoTitle': 'Vicetone &amp; Tony Igy - Astronomia', 'yt_url': 'https://www.youtube.com/watch?v=iLBBRuVDOo4', 'yt_view_counts': '51756866', 'yt_like_counts': '1245465', 'yt_dislike_counts': '11312', 'yt_comment_counts': '61009'}, 'top_3': {'track_id': 188776445, 'track_name': '少年', 'album_name': '少年', 'artist_name': '梦然', 'lyrics': '换种生活 让自己变得快乐\n放弃执着 天气就会变得不错\n每次走过 都是一次收获\n还等什么 做对的选择\n过去的 就让它过去吧\n别管那是一个玩笑还是谎话\n路在脚下 其实并不复杂 只要记得你是你呀\n\n我还是从前那个少年\n没有一丝丝改变\n时间只不过是考验\n种在心中信念丝毫未减\n眼前这个少年\n还是最初那张脸\n面前再多艰险不退却\nSay never never give up\nLike a fighter\n\n换种生活 让自己变得快乐\n放弃执着 天气就会变得不错\n每次走过 都是一次收获\n...\n\n******* This Lyrics is NOT for Commercial use *******\n(1409619538131)', 'lyrics_language': 'not available', 'yt_videoID': 'LGzDEu7Sh5g', 'yt_videoTitle': '【動態歌詞】少年 - 夢然 (Mira) 『我還是從前那個少年 沒有一絲絲改變』', 'yt_url': 'https://www.youtube.com/watch?v=LGzDEu7Sh5g', 'yt_view_counts': '7053798', 'yt_like_counts': '67789', 'yt_dislike_counts': '985', 'yt_comment_counts': '1842'}, 'top_4': {'track_id': 181938434, 'track_name': '芒种', 'album_name': '芒种', 'artist_name': '音阙诗听 feat. 赵方婧', 'lyrics': '一想到你我就\n空恨别梦久\n烧去纸灰埋烟柳\n\n于鲜活的枝丫\n凋零下的⽆暇\n是收获谜底的代价\n\n余晖沾上 远⾏⼈的发\n他洒下⼿中牵挂\n于桥下\n前世迟来者\n掌⼼刻\n你眼中烟波滴落一滴墨\n若佛说\n放下执着\n...\n\n******* This Lyrics is NOT for Commercial use *******\n(1409619538131)', 'lyrics_language': 'not available', 'yt_videoID': 'q2WvTaqe9zU', 'yt_videoTitle': '《芒种》音阙诗听/赵方婧 官方高画质 Official HD MV丨Grain in Ear丨Mang Chủng', 'yt_url': 'https://www.youtube.com/watch?v=q2WvTaqe9zU', 'yt_view_counts': '3299879', 'yt_like_counts': '136973', 'yt_dislike_counts': '843', 'yt_comment_counts': '3736'}, 'top_5': {'track_id': 187906159, 'track_name': '想見你想見你想見你 (電視劇《想見你》片尾曲)', 'album_name': 'Miss You 3000 - Single', 'artist_name': '八三夭', 'lyrics': '當愛情遺落成遺跡 用象形刻劃成回憶\n想念幾個世紀 才是刻骨銘心？\n\n若能回到冰河時期 多想把你抱緊處理\n你的笑多療癒 讓人生也甦醒\n\n失去 你的風景 像座廢墟 像失落文明\n能否 一場奇蹟 一線生機 能不能 有再一次 相遇\n\n想見你 只想見你 未來過去 我只想見你\n穿越了 千個萬個 時間線裡 人海裡相依\n用盡了 邏輯心機 推理愛情 最難解的謎\n會不會 妳也 和我一樣 在等待一句 我願意\n\n任時光更迭了四季 任宇宙物換或星移\n...\n\n******* This Lyrics is NOT for Commercial use *******\n(1409619538131)', 'lyrics_language': 'not available', 'yt_videoID': '4iRupuNet3Q', 'yt_videoTitle': '八三夭 831【想見你想見你想見你 Miss You 3000】電視劇「想見你상견니」片尾曲 Official Music Video', 'yt_url': 'https://www.youtube.com/watch?v=4iRupuNet3Q', 'yt_view_counts': '16021184', 'yt_like_counts': '121470', 'yt_dislike_counts': '1832', 'yt_comment_counts': '3288'}}}
-# country_list = {'afghanistan': 'af', 'aland islands': 'ax', 'albania': 'al', 'algeria': 'dz', 'american samoa': 'as', 'andorra': 'ad', 'angola': 'ao', 'anguilla': 'ai', 'antarctica': 'aq', 'antigua and barbuda': 'ag', 'argentina': 'ar', 'armenia': 'am', 'aruba': 'aw', 'australia': 'au', 'austria': 'at', 'azerbaijan': 'az', 'bahamas': 'bs', 'bahrain': 'bh', 'bangladesh': 'bd', 'barbados': 'bb', 'belarus': 'by', 'belgium': 'be', 'belize': 'bz', 'benin': 'bj', 'bermuda': 'bm', 'bhutan': 'bt', 'bolivia': 'bo', 'bonaire, saint eustatius and saba': 'bq', 'bosnia and herzegovina': 'ba', 'botswana': 'bw', 'bouvet island': 'bv', 'brazil': 'br', 'british indian ocean territory': 'io', 'british virgin islands': 'vg', 'brunei': 'bn', 'bulgaria': 'bg', 'burkina faso': 'bf', 'burundi': 'bi', 'cambodia': 'kh', 'cameroon': 'cm', 'canada': 'ca', 'cape verde': 'cv', 'cayman islands': 'ky', 'central african republic': 'cf', 'chad': 'td', 'chile': 'cl', 'china': 'cn', 'christmas island': 'cx', 'cocos islands': 'cc', 'colombia': 'co', 'comoros': 'km', 'cook islands': 'ck', 'costa rica': 'cr', 'croatia': 'hr', 'cuba': 'cu', 'curacao': 'cw', 'cyprus': 'cy', 'czech republic': 'cz', 'democratic republic of the congo': 'cd', 'denmark': 'dk', 'djibouti': 'dj', 'dominica': 'dm', 'dominican republic': 'do', 'east timor': 'tl', 'ecuador': 'ec', 'egypt': 'eg', 'el salvador': 'sv', 'equatorial guinea': 'gq', 'eritrea': 'er', 'estonia': 'ee', 'ethiopia': 'et', 'falkland islands': 'fk', 'faroe islands': 'fo', 'fiji': 'fj', 'finland': 'fi', 'france': 'fr', 'french guiana': 'gf', 'french polynesia': 'pf', 'french southern territories': 'tf', 'gabon': 'ga', 'gambia': 'gm', 'georgia': 'ge', 'germany': 'de', 'ghana': 'gh', 'gibraltar': 'gi', 'greece': 'gr', 'greenland': 'gl', 'grenada': 'gd', 'guadeloupe': 'gp', 'guam': 'gu', 'guatemala': 'gt', 'guernsey': 'gg', 'guinea': 'gn', 'guinea-bissau': 'gw', 'guyana': 'gy', 'haiti': 'ht', 'heard island and mcdonald islands': 'hm', 'honduras': 'hn', 'hong kong': 'hk', 'hungary': 'hu', 'iceland': 'is', 'india': 'in', 'indonesia': 'id', 'iran': 'ir', 'iraq': 'iq', 'ireland': 'ie', 'isle of man': 'im', 'israel': 'il', 'italy': 'it', 'ivory coast': 'ci', 'jamaica': 'jm', 'japan': 'jp', 'jersey': 'je', 'jordan': 'jo', 'kazakhstan': 'kz', 'kenya': 'ke', 'kiribati': 'ki', 'kosovo': 'xk', 'kuwait': 'kw', 'kyrgyzstan': 'kg', 'laos': 'la', 'latvia': 'lv', 'lebanon': 'lb', 'lesotho': 'ls', 'liberia': 'lr', 'libya': 'ly', 'liechtenstein': 'li', 'lithuania': 'lt', 'luxembourg': 'lu', 'macao': 'mo', 'macedonia': 'mk', 'madagascar': 'mg', 'malawi': 'mw', 'malaysia': 'my', 'maldives': 'mv', 'mali': 'ml', 'malta': 'mt', 'marshall islands': 'mh', 'martinique': 'mq', 'mauritania': 'mr', 'mauritius': 'mu', 'mayotte': 'yt', 'mexico': 'mx', 'micronesia': 'fm', 'moldova': 'md', 'monaco': 'mc', 'mongolia': 'mn', 'montenegro': 'me', 'montserrat': 'ms', 'morocco': 'ma', 'mozambique': 'mz', 'myanmar': 'mm', 'namibia': 'na', 'nauru': 'nr', 'nepal': 'np', 'netherlands': 'nl', 'netherlands antilles': 'an', 'new caledonia': 'nc', 'new zealand': 'nz', 'nicaragua': 'ni', 'niger': 'ne', 'nigeria': 'ng', 'niue': 'nu', 'norfolk island': 'nf', 'north korea': 'kp', 'northern mariana islands': 'mp', 'norway': 'no', 'oman': 'om', 'pakistan': 'pk', 'palau': 'pw', 'palestinian territory': 'ps', 'panama': 'pa', 'papua new guinea': 'pg', 'paraguay': 'py', 'peru': 'pe', 'philippines': 'ph', 'pitcairn': 'pn', 'poland': 'pl', 'portugal': 'pt', 'puerto rico': 'pr', 'qatar': 'qa', 'republic of the congo': 'cg', 'reunion': 're', 'romania': 'ro', 'russia': 'ru', 'rwanda': 'rw', 'saint barthelemy': 'bl', 'saint helena': 'sh', 'saint kitts and nevis': 'kn', 'saint lucia': 'lc', 'saint martin': 'mf', 'saint pierre and miquelon': 'pm', 'saint vincent and the grenadines': 'vc', 'samoa': 'ws', 'san marino': 'sm', 'sao tome and principe': 'st', 'saudi arabia': 'sa', 'senegal': 'sn', 'serbia': 'rs', 'serbia and montenegro': 'cs', 'seychelles': 'sc', 'sierra leone': 'sl', 'singapore': 'sg', 'sint maarten': 'sx', 'slovakia': 'sk', 'slovenia': 'si', 'solomon islands': 'sb', 'somalia': 'so', 'south africa': 'za', 'south georgia and the south sandwich islands': 'gs', 'south korea': 'kr', 'south sudan': 'ss', 'spain': 'es', 'sri lanka': 'lk', 'sudan': 'sd', 'suriname': 'sr', 'svalbard and jan mayen': 'sj', 'swaziland': 'sz', 'sweden': 'se', 'switzerland': 'ch', 'syria': 'sy', 'taiwan': 'tw', 'tajikistan': 'tj', 'tanzania': 'tz', 'thailand': 'th', 'togo': 'tg', 'tokelau': 'tk', 'tonga': 'to', 'trinidad and tobago': 'tt', 'tunisia': 'tn', 'turkey': 'tr', 'turkmenistan': 'tm', 'turks and caicos islands': 'tc', 'tuvalu': 'tv', 'u.s. virgin islands': 'vi', 'uganda': 'ug', 'ukraine': 'ua', 'united arab emirates': 'ae', 'united kingdom': 'gb', 'united states': 'us', 'united states minor outlying islands': 'um', 'uruguay': 'uy', 'uzbekistan': 'uz', 'vanuatu': 'vu', 'vatican': 'va', 'venezuela': 've', 'vietnam': 'vn', 'wallis and futuna': 'wf', 'western sahara': 'eh', 'yemen': 'ye', 'zambia': 'zm', 'zimbabwe': 'zw'}
-# for i, v in track_dict.items():
-# 	for j, w in v.items():
-# 		print(i)
-# 		print(j)
-# 		print(w)
-# 		print('\n')
-
-
 from flask import Flask, render_template, request
 import sqlite3
 import plotly.graph_objects as go
 
 app = Flask(__name__)
 
-
-def get_countries():
+def connect_db(q):
 	conn = sqlite3.connect('country_song.sqlite')
 	cur = conn.cursor()
+	results = cur.execute(q).fetchall()
+	conn.close()
+
+	return results
+
+def get_countries():
+
 	q = f'''
 		SELECT c.countries
 		FROM Countries as c
 	'''
-	results = cur.execute(q).fetchall()
-	conn.close()
+	results = connect_db(q)
+	
 	return results
 
 def get_top_songs(country):
-	conn = sqlite3.connect('country_song.sqlite')
-	cur = conn.cursor()
 	q = f'''
 		SELECT c.countries, v.id, v.title, v.artist_Name, v.album, v.url
 		FROM Countries as c
@@ -37,20 +31,83 @@ def get_top_songs(country):
 		ON v.CountryId = c.Id
 		WHERE c.Countries = '{country}'
 	'''
-	results = cur.execute(q).fetchall()
-	conn.close()
+	results = connect_db(q)
 	return results
-def get_single_song(song_id):
-	conn = sqlite3.connect('country_song.sqlite')
-	cur = conn.cursor()
+
+def get_lyrics(song_id):
+
 	q = f'''
 		SELECT v.id, v.title, v.lyrics, v.embed_url
 		FROM Videos as v
 		WHERE id = '{song_id}'
 		'''
-	results = cur.execute(q).fetchall()
-	conn.close()
+	results = connect_db(q)
 	return results	
+
+def get_multiple_songs_stats(id_list, check_view = False, check_like = False, check_dislike = False, check_comment = False):
+	q_select = 	f'''SELECT v.id, v.title'''
+	if (check_view):
+		q_select += "," + "v.views"
+	if (check_like):
+		q_select += "," + "v.likes"
+	if (check_dislike):
+		q_select += "," + "v.dislikes"
+	if (check_comment):
+		q_select += "," + "v.comment_count"
+
+	q_rest = f'''
+		FROM Videos as v
+		WHERE id IN {id_list}
+		'''
+	q_rest = q_rest.replace('[', '(')
+	q_rest = q_rest.replace(']', ')')
+	q = q_select + q_rest
+	results = connect_db(q)
+	return results
+
+def get_song_view(id_list):
+	q = f'''
+		SELECT v.id, v.title, v.views
+		FROM Videos as v
+		WHERE id IN {id_list}
+		'''
+	q = q.replace('[', '(')
+	q = q.replace(']', ')')
+
+	results = connect_db(q)
+	return results
+def get_song_like(id_list):
+	q = f'''
+		SELECT v.id, v.title, v.likes
+		FROM Videos as v
+		WHERE id IN {id_list}
+		'''
+	q = q.replace('[', '(')
+	q = q.replace(']', ')')
+	results = connect_db(q)
+	return results
+
+def get_song_dislike(id_list):
+	q = f'''
+		SELECT v.id, v.title, v.dislikes
+		FROM Videos as v
+		WHERE id IN {id_list}
+		'''
+	q = q.replace('[', '(')
+	q = q.replace(']', ')')
+	results = connect_db(q)
+	return results
+def get_song_comment(id_list):
+	q = f'''
+		SELECT v.id, v.title, v.comment_count
+		FROM Videos as v
+		WHERE id IN {id_list}
+		'''
+	q = q.replace('[', '(')
+	q = q.replace(']', ')')
+	results = connect_db(q)
+	return results
+
 
 @app.route('/')
 def index():
@@ -76,12 +133,101 @@ def top_congs_compare(country):
 
 @app.route('/single_song/<country>/<song_id>/lyric')
 def single_song_lyric(country, song_id):
-	single_song = get_single_song(int(song_id))
+	single_song = get_lyrics(int(song_id))
 	return render_template('single_song_lyric.html',song_name=single_song[0][1],lyric=single_song[0][2], embed_url=single_song[0][3], country=country)
 
-@app.route('/top_songs/<country>/results')
-def song_compare(country):
-	return('top_songs_compare_results')
+@app.route('/top_songs/<country>/results', methods=['POST'])
+def song_compare_handle_form(country):
+	top_song_list = get_top_songs(country)
+	origina_country_song_id_list =[]
+
+	# get all the song ids in the country
+	for i in top_song_list:
+		origina_country_song_id_list.append(i[1])
+
+	# check if the song is checked in the form
+	check_song_list = {}
+	for i in origina_country_song_id_list:
+		check_song_list[i]= str(i) in request.form.keys()
+
+	# getting only the song ids that are checked in the form
+	checked_song_true_list = []
+	for i, v in check_song_list.items():
+		if v == True:
+			checked_song_true_list.append(i)
+
+	check_view = "view" in request.form.keys()
+	check_like = "like" in request.form.keys()
+	check_dislike = "dislike" in request.form.keys()
+	check_comment = "comment" in request.form.keys()
+	songs_stats=get_multiple_songs_stats(checked_song_true_list,check_view,check_like,check_dislike,check_comment)
+	checked_var_dict = {}
+	if (check_view):
+		view = get_song_view(checked_song_true_list)
+	if (check_like):
+		like = get_song_like(checked_song_true_list)
+	if (check_dislike):
+		dislike = get_song_dislike(checked_song_true_list)
+	if (check_comment):
+		comment = get_song_comment(checked_song_true_list)
+
+	plot_results = request.form.get('plot', False)
+
+	if (plot_results):
+		if (check_view):
+			x_vals = [i[1] for i in songs_stats]
+			y_vals = [i[2] for i in view]
+			bars_data = go.Bar(
+				x=x_vals,
+				y=y_vals
+			)
+			basic_layout = go.Layout(title = "View Counts for Different Songs")
+			fig_1 = go.Figure(data=bars_data, layout=basic_layout)
+			div_1 = fig_1.to_html(full_html=False)
+		else:
+			div_1 = ""
+		if (check_like):
+			x_vals = [i[1] for i in songs_stats]
+			y_vals = [i[2] for i in like]
+			bars_data = go.Bar(
+				x=x_vals,
+				y=y_vals
+			)
+			basic_layout = go.Layout(title = "Like Counts for Different Songs")
+
+			fig_2 = go.Figure(data=bars_data, layout=basic_layout)
+			div_2 = fig_2.to_html(full_html=False)
+		else:
+			div_2 = ""
+		if (check_dislike):
+			x_vals = [i[1] for i in songs_stats]
+			y_vals = [i[2] for i in dislike]
+			bars_data = go.Bar(
+				x=x_vals,
+				y=y_vals
+			)
+			basic_layout = go.Layout(title = "Dislike Counts for Different Songs")
+
+			fig_3 = go.Figure(data=bars_data, layout=basic_layout)
+			div_3 = fig_3.to_html(full_html=False)
+		else:
+			div_3 = ""
+		if (check_comment):
+			x_vals = [i[1] for i in songs_stats]
+			y_vals = [i[2] for i in comment]
+			bars_data = go.Bar(
+				x=x_vals,
+				y=y_vals
+			)
+			basic_layout = go.Layout(title = "Comment Counts for Different Songs")
+			fig_4 = go.Figure(data=bars_data, layout=basic_layout)
+			div_4 = fig_4.to_html(full_html=False)
+		else:
+			div_4 = ""
+		return render_template('top_songs_compare_results_plot.html', country=country, plot_div_1 = div_1, plot_div_2 = div_2, plot_div_3 = div_3, plot_div_4 = div_4)
+	else:
+		return render_template('top_songs_compare_results.html', country=country, song_stats=songs_stats ,check_view = check_view, check_like = check_like, check_dislike = check_dislike, check_comment = check_comment)
 
 if __name__ == '__main__':
 	app.run(debug=True)
+	
